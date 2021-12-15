338aa2043499b6535cd4ab1f48d2ca7626b77637
0lgaP <perronolga@gmail.com>
2021-12-15 15:20:29 +0000

    intro to jquery

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ function multiply(a, b) {
   return a * b;
 };
 console.log(multiply(4, 6));
+//  Jquery thangs
+// $('h1#heading1').hide() --> hides anything #=ID, .=class, none=tag
+// $('.heading2').hide()
+// $('p span').css('color', 'red') --> this uses two tags
+// to isolate only the spans within a peragraph tag
+// $('ul#list li:first').css('color', 'red')
+// $('ul#list li:last').css('color', 'green')
+// $('ul#list li:even').css('background-color', 'orange')
+// $('ul#list li:odd').css('background-color', '#cccccc')
+// $('ul#list li:nth-child(3n)').css('list-style', 'none') --> nth!
+// $(':button').hide() --> :=all types of button(or whatever type)
+// $(':text').hide();
+// $('[href]').css('color', 'red'); --> change all href attributes
+// $('a[href="http://yahoo.com"]').css('color', 'green')
+// $('*').hide() --> *=all
+
+
+// Events
+// $(document).ready(function(){ --> document ready needs
+// to be present,and wrap the js or jquery.
+// Otherwise the page will try to execute the
+// code before the page is loaded leading to script not executing
+//   $('#btn1').click(function(){
+//       alert('Button Clicked!')
+//   });
+// });
