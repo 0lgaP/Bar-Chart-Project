d5880c5dfe9931a0234c1f88864e189a18f919b9
0lgaP <perronolga@gmail.com>
2021-12-17 18:28:26 +0000

    added the input, scripted the array logoc

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-  
+
 });
-
-
- 
 
 /*
 ******************************************************
@@ -16,7 +13,7 @@ code will only execute once full doc is loaded
 ------------------------------------------------------
 ******************************************************
 ------------------------------------------------------
-Hide  # = id | . = class | none = tag | 
+Hide  # = id | . = class | none = tag |
 ------------------------------------------------------
       p span = isolate | * = all | [] = attribute
       ------------------------------------------------
@@ -24,13 +21,13 @@ Hide  # = id | . = class | none = tag |
 ------------------------------------------------------
 $('h1#heading1').hide()
 $('.heading2').hide()
-$('p span').css('color', 'red') 
+$('p span').css('color', 'red')
 $('ul#list li:first').css('color', 'red')
 $('ul#list li:last').css('color', 'green')
 $('ul#list li:even').css('background-color', 'orange')
 $('ul#list li:odd').css('background-color', '#cccccc')
 $('ul#list li:nth-child(3n)').css('list-style', 'none')
-$(':button').hide() 
+$(':button').hide()
 $(':text').hide();
 $('[href]').css('color', 'red');
 $('a[href="http://yahoo.com"]').css('color', 'green')
@@ -124,13 +121,13 @@ $('p.para2').toggleClass('myClass');
 ******************************************************
 ******************************************************
 ------------------------------------------------------
-The following commands require google flag 
+The following commands require google flag
 "--allow-file-access-from-files"
 They will not work from an external js file
 ------------------------------------------------------
 ******************************************************
 ------------------------------------------------------
-Populating div with text 
+Populating div with text
 ------------------------------------------------------
 $('#myDiv').html('<h3>Hello Fhqwgads</h3>');
 ------------------------------------------------------
@@ -157,8 +154,8 @@ $('ul').after('<h4> After the UL</h4>');
 ------------------------------------------------------
 ******************************************************
 ------------------------------------------------------
-Empty - removes not only child (and other descendant) 
-elements, but also any text within the set of matched 
+Empty - removes not only child (and other descendant)
+elements, but also any text within the set of matched
 elements
 ------------------------------------------------------
  $('ul').empty();
@@ -166,9 +163,9 @@ elements
 ******************************************************
 ------------------------------------------------------
 .detach() method is the same as .remove(), except that
-.detach() keeps all jQuery data associated with the 
-removed elements. This method is useful when removed 
-elements are to be reinserted into the DOM at a later 
+.detach() keeps all jQuery data associated with the
+removed elements. This method is useful when removed
+elements are to be reinserted into the DOM at a later
 time.
 ------------------------------------------------------
  $('.para2').detach()
@@ -182,10 +179,10 @@ $('a').removeAttr('target')
 ------------------------------------------------------
 ******************************************************
 ------------------------------------------------------
-wrap - Wrap an HTML structure around each element in 
+wrap - Wrap an HTML structure around each element in
 the set of matched elements.
 
-wrapAll - Wrap an HTML structure around all elements 
+wrapAll - Wrap an HTML structure around all elements
 in the set of matched elements.
 ------------------------------------------------------
 $('p').wrap('<h1>')
@@ -211,4 +208,140 @@ var myArr = ['Brad', 'Kelly', 'Nate', 'Jose'];
       $('#users').append('<li>'+val+'</li>')
     });
 ------------------------------------------------------
-/*
+******************************************************
+------------------------------------------------------
+Fade: in, out, toggle
+Fade in: when btn is clicked, tag with id box fades
+after 3000 miliseconds, then the button's text changes
+to: 'Its Gone'
+Fade out: when btn is clicked, tag with id box fades
+in after 3000 miliseconds, then the button's text changes
+to: 'She Back!'
+Toggle: when btn is clicked, tag with id box toggles between
+fade in and fade out, once the button is pressed it's text
+changes to: 'Toggle me more'
+------------------------------------------------------
+$('#btnFadeOut').click(function() {
+      $('#box').fadeOut(3000, function() {
+        $('#btnFadeOut').text('Its Gone')
+      })
+$('#btnFadeIn').click(function() {
+      $('#box').fadeIn(3000, function() {
+        $('#btnFadeIn').text('She Back!')
+      });
+    });
+$('#btnFadeTog').click(function() {
+      $('#box').fadeToggle(1000, function() {
+        $('#btnFadeTog').text('Toggle me more!')
+      });
+    });
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Slide up, down, toggle & stop
+------------------------------------------------------
+$('#btnSlideUp').click(function() {
+      $('#box').slideUp(3000)
+    });
+$('#btnSlideDown').click(function() {
+      $('#box').slideDown(3000)
+    });
+$('#btnSlideTog').click(function() {
+      $('#box').slideToggle(1000)
+    });
+$('#btnStop').click(function() {
+      $('#box').stop()
+    });
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Animation: move around on button with id 'moveAround'
+animating a div with the id 'box2'
+------------------------------------------------------
+$('#moveAround').click(function() {
+      var box = $('#box2');
+      box.animate({
+        left: 300
+      });
+      box.animate({
+        top: 300
+      });
+      box.animate({
+        left: 0,
+        top: 300
+      });
+      box.animate({
+        left: 0,
+        top: 0
+      });
+    });
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Animation: left, right
+------------------------------------------------------
+$('#moveLeft').click(function() {
+      $('#box2').animate({
+        left: 500,
+        height: '300px',
+        width: '300px',
+        opacity: '0.5'
+      });
+    });
+$('#moveRight').click(function() {
+      $('#box2').animate({
+        left: 0,
+        height: '100px',
+        width: '100px',
+        opacity: '1'
+      });
+    });
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Create a new DOM Element, add style, add text & append
+to body
+------------------------------------------------------
+var DOMobject = document.createElement('div');
+    DOMobject.id = "myDiv";
+    DOMobject.style.width = "100px";
+    DOMobject.style.height = "100px";
+    DOMobject.style.background = "pink"
+    DOMobject.innerText = "Le div"
+    document.body.appendChild(DOMobject);
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Create new DOM element, add text & append to parent
+by id
+------------------------------------------------------
+var spanElement = document.createElement('span');
+    spanElement.innerText = "This is a span"
+    document.getElementById('myDiv').appendChild(spanElement)
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+Functionality of BArChartProject
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+1. Element with the id submit is called .on 'click
+2. Console logs "Clicked" when button is pressed
+3. Data variable is updated to id's .val()
+------------------------------------------------------
+var x = [];
+    $('#submit').on('click', function(){
+      console.log("Clicked")
+      var data = $('#arrValues').val();
+      if(isNaN(parseInt(data, 10))){
+        alert("Please only use numerals")
+      } else {
+        var array = JSON.parse("["+data+"]");
+        x.push(array);
+        console.log(x)
+      };
+------------------------------------------------------
+******************************************************
+------------------------------------------------------
+------------------------------------------------------
+*/
